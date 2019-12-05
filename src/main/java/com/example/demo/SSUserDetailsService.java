58390e9ef15fe597c9af71940e6f29903490f3a5
fenglin098 <feng.lin098@gmail.com>
2019-12-05 13:53:19 -0500

    final

--- src/main/java/com/example/demo/SSUserDetailsService.java
+++ src/main/java/com/example/demo/SSUserDetailsService.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Set;
 
 @Transactional
 @Service
@@ -23,10 +28,20 @@ public class SSUserDetailsService implements UserDetailsService {
             if (user==null){
                 return null;
             }
-        }return new org.springframework.security.core.userdetails.User(
-                user.getUsername(), user.getPassword(), getAuthorities(user)
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(), user.getPassword(), getAuthorities(user)
         );
-
+        }catch (Exception e){
+        throw new UsernameNotFoundException("User not found");
+        }
     }
 
+    private Set<GrantedAuthority> getAuthorities(User user) {
+    Set<GrantedAuthority> authorities = new HashSet<>();
+    for (Role role : user.getRoles()) {
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getRole());
+        authorities.add(grantedAuthority);
+    }
+    return authorities;
+    }
 }
